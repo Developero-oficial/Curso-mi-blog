14035a66c010ddbb6c266f717a0351eb200035ee
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-01-21 14:00:51 -0600

    Se modifica metodo de login para aplicar contrasena con hash segura

--- class/Login/Login.class.php
+++ class/Login/Login.class.php
@@ -18,18 +18,20 @@ class Login
   }
 
   public function setPassword (string $password) {
-    $this->password = $this->con->real_escape_string($password);
+    $this->password = $password;
   }
 
   public function signIn () {
-    $query = "SELECT * FROM `usuario` WHERE email_dev = '$this->email' AND password_dev = '$this->password' ";
+    $query = "SELECT * FROM `usuario` WHERE email_dev = '$this->email'";
     echo $query;
     $res = $this->con->query($query);
-    if ($this->con->affected_rows > 0) 
-      return $res->fetch_array(MYSQLI_ASSOC);
+    if ($this->con->affected_rows > 0) {
+      $row = $res->fetch_array(MYSQLI_ASSOC);
+      if (password_verify($this->password, $row['password_dev']))
+        return $row;
+    }      
     return false;
   }
-
 }
 
 ?>
